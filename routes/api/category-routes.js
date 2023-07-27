c6aceb1b7126c755f1715c9f13895fba6cd21769
JLopez1227 <josuenmilopez27@gmail.com>
2023-07-27 16:02:35 -0500

    Began working on routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  const data = await Category.findAll({include: [Product]})
+  res.json(data);
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  const data = await Category.findOne({where: {id: req.params.id}, include: [Product]})
+  res.json(data);
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  // create a new category`
 });
 
 router.put('/:id', (req, res) => {
