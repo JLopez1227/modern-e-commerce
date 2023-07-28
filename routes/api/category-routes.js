454ed815d19936d72387d1d4105d8d71647377c8
JLopez1227 <josuenmilopez27@gmail.com>
2023-07-27 21:57:12 -0500

    Worked on Category Routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,15 +13,31 @@ router.get('/:id', async (req, res) => {
   res.json(data);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category`
+  const data = await Category.create(req.body)
+  res.json(data);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  const data = await Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+    
+  })
+  res.json(data);
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  const data = await Category.destroy({
+    where: {
+      id: req.params.id
+    }
+    
+  })
+  res.json(data);
   // delete a category by its `id` value
 });
 
